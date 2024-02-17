c921968e9579a66b8d8fa6a6d94f142681c641f2
devnshankar <subhashankarbehera7@gmail.com>
2024-02-17 16:06:55 +0530

    added clusterization feature

--- src/index.js
+++ src/index.js
@@ -7,52 +7,50 @@ import userRouter from "./routes/user/user.routes.js";
 import astrologerRouter from "./routes/astrologer/astrologer.routes.js";
 import adminRouter from "./routes/admin/admin.routes.js";
 import { Server } from "socket.io";
+import { nodeClusterizer } from "./middlewares/clusterizer.middlewares.js";
 
 // constants and config
-const app = express();
-const server = createServer(app);
-dotenv.config();
-const PORT = process.env.PORT;
-// CORS CONFIG
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET, POST, PUT, DELETE"],
-    credentials: true,
-  })
-);
-
-// EXPRESS JSON BODY PARSER
-app.use(express.json());
-
-// ROUTES FOR USER
-app.use("/user", userRouter)
-
-// ROUTES FOR ASTROLOGER
-app.use("/astrologer", astrologerRouter)
-
-// ROUTES FOR ADMIN
-app.use("/admin", adminRouter)
 
 async function main() {
   try {
-      // Listener
-      server.listen(PORT, () => {
-        console.log(`Server is running on port http://localhost:${PORT}`)
+    const app = express();
+    const server = createServer(app);
+    dotenv.config();
+    const PORT = process.env.PORT;
+    // CORS CONFIG
+    app.use(
+      cors({
+        origin: "*",
+        methods: ["GET, POST, PUT, DELETE"],
+        credentials: true,
       })
+    );
 
-      // Routes
-      app.get("/" , (req, res) => {
-        res.send("Welcome to Root")
-      })
+    // EXPRESS JSON BODY PARSER
+    app.use(express.json());
 
+    // ROUTES FOR USER
+    app.use("/user", userRouter);
 
-      
+    // ROUTES FOR ASTROLOGER
+    app.use("/astrologer", astrologerRouter);
+
+    // ROUTES FOR ADMIN
+    app.use("/admin", adminRouter);
+
+    // Listener
+    server.listen(PORT, () => {
+      console.log(`Server is running on port http://localhost:${PORT}`);
+    });
+
+    // Routes
+    app.get("/", (req, res) => {
+      res.send("Welcome to Root");
+    });
   } catch (error) {
     console.log(error);
   }
 }
 
-// main function invocation
-main();
-
+// main function invocation with node clusterization for multithreading
+nodeClusterizer(main);
